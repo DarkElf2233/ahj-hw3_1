722b15dd2a6400000a0a10dad4c37b86722b7576
DarkElf2233 <fd.melnikoff@gmail.com>
2024-02-25 12:22:51 +0300

    Fix event listener error

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,15 @@
 import Game from './game';
-import Click from './click';
 
-const holes = [...document.querySelectorAll('.hole')];
 const holeGame = document.querySelector('.hole-game');
 const holeWithGoblin = document.querySelector('.hole_has-goblin');
 const dead = document.querySelector('.dead');
 const lost = document.querySelector('.lost');
 
-const game = new Game(holeGame, holeWithGoblin);
-const click = new Click(dead, lost);
+const game = new Game(holeGame, holeWithGoblin, dead, lost);
 
-let clicked = false;
-for (const hole of holes) {
-  hole.addEventListener('click', () => {
-    clicked = true;
-    click.checkClickedHole(hole);
-  });
-}
+holeGame.addEventListener('click', (e) => {
+  game.clicked = true;
+  game.checkClickedHole(e.target);
+});
 
-setInterval(() => {
-  game.changeHole();
-  if (!clicked) {
-    click.lost.textContent = parseInt(click.lost.textContent) + 1;
-    if (click.checkPoints() === 'lost') {
-      click.resetGame('Вы проиграли!');
-    }
-  }
-  clicked = false;
-}, 1000);
+game.startGame();
--- src/js/game.js
+++ src/js/game.js
@@ -1,7 +1,11 @@
 export default class Game {
-  constructor(holeGame, holeWithGoblin) {
+  constructor(holeGame, holeWithGoblin, dead, lost) {
     this.holeWithGoblin = holeWithGoblin;
     this.holeGame = holeGame;
+    this.dead = dead;
+    this.lost = lost;
+
+    this.clicked = false;
   }
 
   getRandomNum(currentNum) {
@@ -12,10 +16,52 @@ export default class Game {
     return randomNum;
   }
 
+  resetGame(message) {
+    alert(message);
+    this.dead.textContent = 0;
+    this.lost.textContent = 0;
+  }
+
+  checkPoints() {
+    if (parseInt(this.dead.textContent) === 10) {
+      return 'win';
+    } if (parseInt(this.lost.textContent) === 5) {
+      return 'lost';
+    }
+    return false;
+  }
+
   changeHole() {
     const randomNum = this.getRandomNum(this.holeWithGoblin.dataset.id);
     this.holeWithGoblin.classList.remove('hole_has-goblin');
     this.holeWithGoblin = this.holeGame.querySelector(`[data-id="${randomNum}"]`);
     this.holeWithGoblin.classList.add('hole_has-goblin');
+  }
+
+  checkClickedHole(hole) {
+    if (hole.classList.contains('hole_has-goblin')) {
+      this.dead.textContent = parseInt(this.dead.textContent) + 1;
+      if (this.checkPoints() === 'win') {
+        this.resetGame('Победа!');
+      }
+    } else {
+      this.lost.textContent = parseInt(this.lost.textContent) + 1;
+      if (this.checkPoints() === 'lost') {
+        this.resetGame('Вы проиграли!');
+      }
+    }
+  }
+
+  startGame() {
+    setInterval(() => {
+      this.changeHole();
+      if (!this.clicked) {
+        this.lost.textContent = parseInt(this.lost.textContent) + 1;
+        if (this.checkPoints() === 'lost') {
+          this.resetGame('Вы проиграли!');
+        }
+      }
+      this.clicked = false;
+    }, 1000);
   }
 }
